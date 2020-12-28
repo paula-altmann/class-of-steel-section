5795783aea9023cff4a9e2b3ca4c03743707e60a
paula-altmann <altmannpaula@gmail.com>
2020-12-28 16:54:24 +0100

    Added control of input variables.

--- class_of_steel_section.py
+++ class_of_steel_section.py
@@ -3,10 +3,38 @@ import sys
 
 print("Wprowadź poniższe dane aby wyliczyć klasę elementu. W zapisie dziesiętnym użyj kropki.")
 # pobieranie danych
-t = float(input("Podaj wartość t [mm] = "))
-c = float(input("Podaj wartość c [mm] = "))
-fy = int(input("Podaj wartość fy [MPa] = "))
-a = input("Przekrój ściskany czy zginany? [wpisz s/z]: ")
+while True:
+    try:
+        t = float(input("Podaj wartość t [mm] = "))
+        break
+    except:
+        print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
+        continue
+
+while True:
+    try:
+        c = float(input("Podaj wartość c [mm] = "))
+        if c <= t:
+            print("Podane c jest mniejsze od t - wprowadź poprawną wartość.")
+            continue
+        break
+    except:
+        print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
+        continue
+
+while True:
+    try:
+        fy = int(input("Podaj wartość fy [MPa] = "))
+        break
+    except:
+        print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
+        continue
+
+while True:
+    a = input("Przekrój ściskany czy zginany? [wpisz s/z]: ")
+    if a == "z" or a == "s":
+        break
+    print("Podałeś niewłaściwą literę - wpisz z, jeśli przekrój jest zginany, wpisz s, jeśli przekrój jest ściskany.")
 
 # policzenie c/t oraz epsilon
 b = c/t
@@ -14,20 +42,20 @@ E = math.sqrt((235/fy))
 
 # sprawdzenie warunków
 if a == "z":
-    if c/t <= 72*E:
+    if b <= 72*E:
         k = 1
-    elif c/t <= 83*E:
+    elif b <= 83*E:
         k = 2
-    elif c/t <= 124*E:
+    elif b <= 124*E:
         k = 3
     else:
         k = 4
 elif a == "s":
-    if c/t <= 33*E:
+    if b <= 33*E:
         k = 1
-    elif c/t <= 38*E:
+    elif b <= 38*E:
         k = 2
-    elif c/t <= 42*E:
+    elif b <= 42*E:
         k = 3
     else:
         k = 4
