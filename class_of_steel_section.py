9dec25670e0f7ff84081425b49f197e0b3aecb8f
paula-altmann <altmannpaula@gmail.com>
2020-12-29 13:02:22 +0100

    Added functions to get varibles

--- class_of_steel_section.py
+++ class_of_steel_section.py
@@ -1,40 +1,53 @@
 import math
 import sys
 
-print("Wprowadź poniższe dane aby wyliczyć klasę elementu. W zapisie dziesiętnym użyj kropki.")
+
 # pobieranie danych
-while True:
-    try:
-        t = float(input("Podaj wartość t [mm] = "))
-        break
-    except:
-        print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
-        continue
+def get_float_value(index, t_value):
+    while True:
+        try:
+            x = float(input("Podaj wartość " + index + " [mm] = "))
+            if index == "c" and x <= t_value:
+                print("Podane c jest mniejsze od t - wprowadź poprawną wartość.")
+                continue
+            return x
+        except ValueError:
+            print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
 
-while True:
-    try:
-        c = float(input("Podaj wartość c [mm] = "))
-        if c <= t:
-            print("Podane c jest mniejsze od t - wprowadź poprawną wartość.")
-            continue
-        break
-    except:
-        print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
-        continue
 
-while True:
-    try:
-        fy = int(input("Podaj wartość fy [MPa] = "))
-        break
-    except:
-        print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
-        continue
+def get_int_value(index):
+    while True:
+        try:
+            x = int(input("Podaj wartość " + index + " [MPa] = "))
+            return x
+        except ValueError:
+            print("Podałeś niewłaściwą wartość, wpisz ją ponownie.")
 
-while True:
-    a = input("Przekrój ściskany czy zginany? [wpisz s/z]: ")
-    if a == "z" or a == "s":
-        break
-    print("Podałeś niewłaściwą literę - wpisz z, jeśli przekrój jest zginany, wpisz s, jeśli przekrój jest ściskany.")
+
+def section_force():
+    while True:
+        x = input("Przekrój ściskany czy zginany? [wpisz s/z]: ")
+        if x == "z" or x == "s":
+            return x
+        print("Podałeś niewłaściwą literę. Wpisz z jeśli przekrój jest zginany, wpisz s jeśli przekrój jest ściskany.")
+
+
+def class_of_section(b, E, y1, y2, y3):
+    if b <= y1 * E:
+        return 1
+    elif b <= y2 * E:
+        return 2
+    elif b <= y3 * E:
+        return 3
+    else:
+        return 4
+
+
+print("Wprowadź poniższe dane aby wyliczyć klasę elementu. W zapisie dziesiętnym użyj kropki.")
+t = get_float_value("t", None)
+c = get_float_value("c", t)
+fy = get_int_value("fy")
+a = section_force()
 
 # policzenie c/t oraz epsilon
 b = c/t
@@ -42,27 +55,12 @@ E = math.sqrt((235/fy))
 
 # sprawdzenie warunków
 if a == "z":
-    if b <= 72*E:
-        k = 1
-    elif b <= 83*E:
-        k = 2
-    elif b <= 124*E:
-        k = 3
-    else:
-        k = 4
+    k = class_of_section(b, E, 72, 83, 124)
 elif a == "s":
-    if b <= 33*E:
-        k = 1
-    elif b <= 38*E:
-        k = 2
-    elif b <= 42*E:
-        k = 3
-    else:
-        k = 4
+    k = class_of_section(b, E, 33, 38, 42)
 else:
     print("Wprowadziłeś niepoprawną zmienną - uruchom ponownie program i wybierz s lub z")
     sys.exit(0)
 
 # klasa przekroju
 print("Klasa wprowadzonego przekroju: ", k)
-
